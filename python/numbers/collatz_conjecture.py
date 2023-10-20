5f89e7b5d6ce8435adca7a0c0742f502244cb55a
rodrigocitadin <citadinrodrigo@gmail.com>
2023-10-20 20:02:10 -0300

    dsadjadjlk

---
+++ python/numbers/collatz_conjecture.py
@@ -0,0 +1,17 @@
+def steps(number: int):
+    if number < 1:
+        raise ValueError("Only positive integers are allowed")
+
+    steps = 0
+
+    while number > 1:
+        if number % 2 == 0:
+            number //= 2
+
+        else:
+            number *= 3
+            number += 1
+
+        steps += 1
+
+    return steps
