dfcccff81fe0f538335d06a57e2ba9c7f8d4bb45
rodrigocitadin <citadinrodrigo@gmail.com>
2023-11-23 21:31:26 -0300

    sex

---
+++ csharp/SqueakyClean.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Text;
+
+public static class Identifier
+{
+    private static bool IsGreekLowercase(char c) => (c >= 'α' && c <= 'ω');
+    
+    public static string Clean(string identifier)
+    {
+        StringBuilder stringBuilder = new();
+        bool isAfterDash = false;
+        
+        foreach (char c in identifier)
+        {
+            stringBuilder.Append(c switch
+            {
+                _ when IsGreekLowercase(c) => default,
+                _ when isAfterDash => char.ToUpperInvariant(c),
+                _ when char.IsWhiteSpace(c) => '_',
+                _ when char.IsControl(c) => "CTRL",
+                _ when char.IsLetter(c) => c,
+                _ => default,
+            });
+
+            isAfterDash = c.Equals('-');
+        }
+
+        return stringBuilder.ToString();
+    }
+}
+
