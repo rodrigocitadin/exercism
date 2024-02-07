198b62d9e3e369fa7c221d320a7e1ffc901ca18a
rodrigocitadin <citadinrodrigo@gmail.com>
2024-02-07 09:48:34 -0300

    hello

---
+++ fsharp/LogLevels.fs
@@ -0,0 +1,9 @@
+module LogLevels
+
+let message (logLine: string) : string = logLine.Split(':').[1].Trim()
+
+let logLevel (logLine: string) : string =
+    logLine.Split(']').[0].Trim('[').ToLower()
+
+let reformat (logLine: string) : string =
+    $"{message logLine} ({logLevel logLine})"
