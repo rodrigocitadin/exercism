f11857dd627eeeab1b758e1a1e5f0ac075f491d6
rodrigocitadin <citadinrodrigo@gmail.com>
2024-02-15 21:34:16 -0300

    asjdklasdjkla

---
+++ fsharp/BookingUpForBeauty.fs
@@ -0,0 +1,17 @@
+module BookingUpForBeauty
+
+open System
+
+let schedule (appointmentDateDescription: string) : DateTime =
+    DateTime.Parse(appointmentDateDescription)
+
+let hasPassed (appointmentDate: DateTime) : bool = appointmentDate < DateTime.Now
+
+let isAfternoonAppointment (appointmentDate: DateTime) : bool =
+    appointmentDate.Hour >= 12 && appointmentDate.Hour < 18
+
+let description (appointmentDate: DateTime) : string =
+    $"""You have an appointment on {appointmentDate.ToString "M/d/yyyy h:mm:ss tt"}."""
+
+let anniversaryDate () : DateTime =
+    DateTime(DateTime.Now.Year, 9, 15)
