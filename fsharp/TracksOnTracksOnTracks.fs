9a6f5e47b14b2996ee073d4738ff49c22d6ea07e
rodrigocitadin <citadinrodrigo@gmail.com>
2024-02-17 19:30:09 -0300

    hi

---
+++ fsharp/TracksOnTracksOnTracks.fs
@@ -0,0 +1,18 @@
+module TracksOnTracksOnTracks
+
+let newList: string list = []
+
+let existingList: string list = [ "F#"; "Clojure"; "Haskell" ]
+
+let addLanguage (language: string) (languages: string list) : string list = language :: languages
+
+let countLanguages (languages: string list) : int = languages.Length
+
+let reverseList (languages: string list) : string list = List.rev languages
+
+let excitingList (languages: string list) : bool =
+    match languages with
+    | "F#" :: _
+    | [ _; "F#" ]
+    | [ _; "F#"; _ ] -> true
+    | _ -> false
