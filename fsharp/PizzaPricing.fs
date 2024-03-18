7de4321e2dd7d22a8e867f801905597b2b360d68
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-18 20:50:14 -0300

    hello fs

---
+++ fsharp/PizzaPricing.fs
@@ -0,0 +1,23 @@
+module PizzaPricing
+
+type Pizza =
+    | Margherita
+    | Caprese
+    | Formaggio
+    | ExtraSauce of Pizza
+    | ExtraToppings of Pizza
+
+let rec pizzaPrice (pizza: Pizza) : int =
+    match pizza with
+    | Margherita -> 7
+    | Caprese -> 9
+    | Formaggio -> 10
+    | ExtraSauce pizza -> 1 + pizzaPrice pizza
+    | ExtraToppings pizza -> 2 + pizzaPrice pizza
+
+let orderPrice (pizzas: Pizza list) : int =
+    List.sumBy pizzaPrice pizzas
+    + match pizzas.Length with
+      | 1 -> 3
+      | 2 -> 2
+      | _ -> 0
