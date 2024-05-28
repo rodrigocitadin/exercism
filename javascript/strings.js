4bcd144a1c5365d64452d9cd64a9b1438c7a5ece
rodrigocitadin <citadinrodrigo@gmail.com>
2024-05-28 02:59:19 -0300

    done

---
+++ javascript/strings.js
@@ -0,0 +1,46 @@
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the front door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function frontDoorResponse(line) {
+  return line[0]
+}
+
+/**
+ * Format the password for the front-door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the front door password
+ */
+export function frontDoorPassword(word) {
+  let firstLetter = word[0].toUpperCase()
+  return firstLetter + word.slice(1).toLowerCase()
+}
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the back door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function backDoorResponse(line) {
+  let trimLine = line.trim()
+  return trimLine[trimLine.length - 1]
+}
+
+/**
+ * Format the password for the back door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the back door password
+ */
+export function backDoorPassword(word) {
+  let capWord = frontDoorPassword(word)
+  return capWord + ", please"
+}
