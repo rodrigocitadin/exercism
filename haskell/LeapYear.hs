0ab6d20294ec980797530e487ab44cfa159e1903
rodrigocitadin <citadinrodrigo@gmail.com>
2025-02-11 15:39:40 -0300

    more haskellian solution

--- haskell/LeapYear.hs
+++ haskell/LeapYear.hs
@@ -1,7 +1,14 @@
 module LeapYear (isLeapYear) where
 
+-- isLeapYear :: Integer -> Bool
+-- isLeapYear year =
+--   rem year 4 == 0
+--     && rem year 100 /= 0
+--     || rem year 400 == 0
+
 isLeapYear :: Integer -> Bool
-isLeapYear year =
-  rem year 4 == 0
-    && rem year 100 /= 0
-    || rem year 400 == 0
+isLeapYear year
+  | rem year 400 == 0 = True
+  | rem year 100 == 0 = False
+  | rem year 4 == 0 = True
+  | otherwise = False
